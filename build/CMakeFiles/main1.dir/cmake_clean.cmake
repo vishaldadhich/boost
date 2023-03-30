cb272626d62a173efcacb26a1c25a82b5c87db5a
vishal <vishaldadhichsrg1997@gmail.com>
2023-03-30 12:38:40 +0530

    1st commit

---
+++ build/CMakeFiles/main1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main1.dir/main.cpp.obj"
+  "CMakeFiles/main1.dir/main.cpp.obj.d"
+  "libmain1.dll.a"
+  "main1.exe"
+  "main1.exe.manifest"
+  "main1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
